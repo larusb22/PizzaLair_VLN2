518411882360ffff45bd7db55b00007b18c4a99e
Borgarinn02 <112634893+Borgarinn02@users.noreply.github.com>
2023-05-04 13:42:38 +0000

    offers

--- offers/views.py
+++ offers/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+offers = [
+    {'name': '2 For 1 ', 'description': '', 'price': 9.99},
+    {'name': 'Meat Fiesta', 'description': '', 'price': 10.99},
+]
+# Create your views here.
 def index(request):
-    return render(request, 'offers/index.html')
+    return render(request, 'offers/index.html', context={'offers': offers})
