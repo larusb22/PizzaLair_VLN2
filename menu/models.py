c5d60fa92d2a77f0eb9a44832ac299e8afde947c
joejoe02 <johannagustha@gmail.com>
2023-05-05 15:42:42 +0000

    ll

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
+
 # Create your models here.
+class MenuRating(models.Model):
+    rating = models.FloatField()
+    total_ratings = models.IntegerField()
+
+
+class MenuType(models.Model):  # Product Type (spicy eða eh þannig)
+    name = models.CharField(max_length=255)
+
+
+class MenuToppingType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class MenuTopping(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.ForeignKey(MenuToppingType, on_delete=models.CASCADE)  # Kannski hafa foreign key í Type klasann
+
+
+class MenuSize(models.Model):
+    name = models.CharField(max_length=255)  # fyrir small, medium og large
+
+
+class MenuProduct(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
+    rating = models.ForeignKey(MenuRating, on_delete=models.CASCADE)
+    type = models.ForeignKey(MenuType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuProductTopping(models.Model):
+    menu = models.ForeignKey(MenuProduct, on_delete=models.CASCADE)
+    topping = models.ForeignKey(MenuTopping, on_delete=models.CASCADE)
+
+
+class MenuProductPrice(models.Model):  # spurja kennara út í
+    price = models.FloatField()
+    menu = models.ForeignKey(MenuProduct, on_delete=models.CASCADE)
+    size = models.ForeignKey(MenuSize, on_delete=models.CASCADE)
+
+
+class MenuImage(models.Model):
+    image = models.CharField(max_length=9999)
+    menu = models.ForeignKey(MenuProduct, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image
---
+++ population_script.sql
@@ -0,0 +1,67 @@
+INSERT INTO menu_menusize (name) VALUES ('Small');
+INSERT INTO menu_menusize (name) VALUES ('Medium');
+INSERT INTO menu_menusize (name) VALUES ('Large');
+
+INSERT INTO menu_menutype (name) VALUES ('Spicy');
+INSERT INTO menu_menutype (name) VALUES ('Vegan');
+INSERT INTO menu_menutype (name) VALUES ('Vegeterian');
+INSERT INTO menu_menutype (name) VALUES ('Meat lovers');
+
+
+INSERT INTO menu_menutoppingtype (name) VALUES ('Meat');
+INSERT INTO menu_menutoppingtype (name) VALUES ('Greens');
+INSERT INTO menu_menutoppingtype (name) VALUES ('Cheese');
+INSERT INTO menu_menutoppingtype (name) VALUES ('Sauce');
+INSERT INTO menu_menutoppingtype (name) VALUES ('Spice');
+
+
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Pepperoni', 1);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Ham', 1);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Bacon', 1);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Chicken', 1);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Pulled pork', 1);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Pineapple', 2);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Dates', 2);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Chili', 2);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Bell Pepper', 2);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Jalapeno', 2);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Mushrooms', 2);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Onion', 2);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Cheddar', 3);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Creamcheese', 3);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Vegancheese', 3);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Mozzarella', 3);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Black pepper', 4);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Oregano', 4);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Fresh basil', 4);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('BBQ', 5);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Chipotle', 5);
+INSERT INTO menu_menutopping (name, type_id) VALUES ('Berneise', 5);
+
+
+
+INSERT INTO menu_menuproduct (name, description, rating_id, type_id) VALUES ('Meat lovers madness', 'This pizza is what started Pizza Lair, its a fan favourite and will not leave you hanging.', 1, 4);
+INSERT INTO menu_menuproduct (name, description, rating_id, type_id) VALUES ('The Spicy Southwest', 'This is an Alabama favourite, made with love', 2, 1);
+INSERT INTO menu_menuproduct (name, description, rating_id, type_id) VALUES ('Vegan Savior', 'This pizza is for all my vegans out there', 3, 2);
+INSERT INTO menu_menuproduct (name, description, rating_id, type_id)  VALUES ('Vegetarian Warrior', 'This pizza lives up to its name, trust me with this one', 4, 3);
+
+
+INSERT INTO menu_menurating (rating, total_ratings) VALUES (5, 1);
+
+INSERT INTO menu_menuimage (image, menu_id) VALUES ('https://api.salescloud.is/sites/default/files/styles/large/public/products/dsc05660-3.jpg?itok=JUNeaQf1', 1);
+INSERT INTO menu_menuimage (image, menu_id) VALUES ('https://api.salescloud.is/sites/default/files/styles/large/public/products/dsc05670-3.jpg?itok=VnsjeCuy', 2);
+
+
+
+INSERT INTO menu_menuproducttopping (menu_id, topping_id) VALUES (1, 23);
+INSERT INTO menu_menuproducttopping (menu_id, topping_id) VALUES (1, 24);
+INSERT INTO menu_menuproducttopping (menu_id, topping_id) VALUES (1, 25);
+INSERT INTO menu_menuproducttopping (menu_id, topping_id) VALUES (1, 32);
+INSERT INTO menu_menuproducttopping (menu_id, topping_id) VALUES (1, 36);
+INSERT INTO menu_menuproducttopping (menu_id, topping_id) VALUES (1, 39);
+INSERT INTO menu_menuproducttopping (menu_id, topping_id) VALUES (1, 42);
+
+
+INSERT INTO menu_menuproductprice (price, menu_id, size_id) VALUES (1990, 1, 1);
+INSERT INTO menu_menuproductprice (price, menu_id, size_id) VALUES (2590, 1, 2);
+INSERT INTO menu_menuproductprice (price, menu_id, size_id) VALUES (3490, 1, 3);
