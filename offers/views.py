bf3f4b7a24e81e3d67f85de52b1355b44ee29efb
Borgarinn02 <112634893+Borgarinn02@users.noreply.github.com>
2023-05-09 11:49:47 +0000

    offfers
    offers

--- offers/views.py
+++ offers/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 offers = [
-    {'name': '2 For 1 ', 'description': '', 'price': 9.99},
-    {'name': 'Meat Fiesta', 'description': '', 'price': 10.99},
+    {'name': '2 For 1 ', 'description': 'Buy 1 and get 2', 'price': 14.99},
+    {'name': 'Meat Fiesta', 'description': 'A lot of meat', 'price': 15.99},
 ]
 # Create your views here.
 def index(request):
