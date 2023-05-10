98b1fa9e7d7c51b33e76b07e4ec3b12f2bf3cf22
Larus Bergmann Hakonarson <larusbergmann@Laruss-MacBook-Pro.local>
2023-05-10 23:52:35 +0000

    added the login and register button and almost finished function

--- PizzaLair/settings.py
+++ PizzaLair/settings.py
@@ -137,3 +137,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'account/login'
+LOGIN_REDIRECT_URL = 'account/profile'  #þarf kannski ekki
